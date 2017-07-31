6b8a920ab91754da23da9fbfe6a7a29b76733821
Jeff Newman <jnewman@camsys.com>
2017-07-31 12:19:55 -0500

    return a string

--- pines/hardware_info.py
+++ pines/hardware_info.py
@@ -5,17 +5,21 @@ import re
 def processor_name():
 	"""Get a descriptive name of the CPU on this computer"""
 	if platform.system() == "Windows":
-		return platform.processor()
+		result = platform.processor()
 	elif platform.system() == "Darwin":
 		command =("sysctl", "-n", "machdep.cpu.brand_string")
-		return subprocess.check_output(command).strip()
+		result = subprocess.check_output(command).strip()
 	elif platform.system() == "Linux":
 		command = ("cat", "/proc/cpuinfo")
 		all_info = subprocess.check_output(command, shell=True).strip()
 		for line in all_info.split("\n"):
 			if "model name" in line:
-				return re.sub( ".*model name.*:", "", line,1)
-	return ""
+				result = re.sub( ".*model name.*:", "", line,1)
+	else:
+		result = "unknown system"
+	if isinstance(result, bytes):
+		return result.decode('UTF-8')
+	return result
 
 def node():
 	return platform.node()
