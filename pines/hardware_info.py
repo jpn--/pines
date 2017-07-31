083291b9953a9cdaa83bd8b5124fe455ab24b22f
Jeff Newman <jnewman@camsys.com>
2017-07-31 10:41:06 -0500

    hardware info

---
+++ pines/hardware_info.py
@@ -0,0 +1,21 @@
+import platform
+import subprocess
+import re
+
+def processor_name():
+	"""Get a descriptive name of the CPU on this computer"""
+	if platform.system() == "Windows":
+		return platform.processor()
+	elif platform.system() == "Darwin":
+		command =("sysctl", "-n", "machdep.cpu.brand_string")
+		return subprocess.check_output(command).strip()
+	elif platform.system() == "Linux":
+		command = ("cat", "/proc/cpuinfo")
+		all_info = subprocess.check_output(command, shell=True).strip()
+		for line in all_info.split("\n"):
+			if "model name" in line:
+				return re.sub( ".*model name.*:", "", line,1)
+	return ""
+
+def node():
+	return platform.node()
