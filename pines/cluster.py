c2d6fe6da06cde0c885ef0566a1eb58b8f3bb0f3
Jeff Newman <jnewman@camsys.com>
2017-07-31 13:42:38 -0500

    json emitters

--- pines/cluster.py
+++ pines/cluster.py
@@ -4,6 +4,8 @@ from . import egnyte as pe
 import re
 import time
 import json
+import pandas
+import os
 from .logger import flogger
 
 clog = flogger(label='CLUSTER')
@@ -137,3 +139,83 @@ def unclaimed_jobs(jobs_dir, results_dir=None):
 				clog(f'{fi.name} is not a job')
 	raise StopIteration
 
+
+from .repeater import create_csv_repeat_set, external_repeater, call_me
+
+
+def next_job_number(egnyte_path):
+	c = re.compile('^([0-9]+)\\s.+')
+	seen_max = 0
+	eg_folder = pe.client.folder(pe.pth(egnyte_path))
+	pe._load_obj(eg_folder)
+	for fo in eg_folder.folders:
+		match = c.match(fo.name)
+		if match:
+			seen_max = max(seen_max, int(match.group(1)))
+	for fi in eg_folder.files:
+		match = c.match(fi.name)
+		if match:
+			seen_max = max(seen_max, int(match.group(1)))
+	return seen_max+1
+
+
+
+def json_emitter(kwarg_file, **kwargs):
+	"""
+	Use an external CSV file to iterate over keyword args passed to a function.
+
+	Parameters
+	----------
+	func : callable
+		This function gets called once for each row of the CSV file
+	kwarg_file : str or file-like
+		A csv file containing keywork args (simple data types as read by pandas)
+
+	Other Parameters
+	----------------
+	args
+		Positional arguments always passed to `func`
+	kwargs
+		Common keyword arguments always passed to `func`
+
+	Returns
+	-------
+	list
+		A list containing the return value of `func` for each row
+		of the csv file.
+	"""
+	result = []
+	df = pandas.read_csv(kwarg_file)
+	direct_kw = {}
+	indirect_kw = {}
+	for k, v in kwargs.items():
+		if isinstance(v, call_me):
+			indirect_kw[k] = v
+		else:
+			direct_kw[k] = v
+	for row in df.iterrows():
+		local_kwargs = row[1].to_dict()
+		indirect_kwargs = {k: v() for k, v in indirect_kw.items()}
+		to_json = dict(**direct_kw, **indirect_kwargs, **local_kwargs)
+		result.append(to_json)
+	return result
+
+
+def looping_json_emitter(egnyte_dir, descrip, *loopers, **kwargs):
+	buffer = create_csv_repeat_set(*loopers, filename=None, return_buffer=True)
+	jsons = json_emitter(buffer, **kwargs)
+	min_job_number = next_job_number(egnyte_dir)
+	for jobnum, j in enumerate(jsons):
+		clog(f'uploading job {min_job_number+jobnum}')
+		pe.upload_dict_json(j, f'{min_job_number+jobnum} {descrip}.json', egnyte_dir)
+	clog(f'looping_json_emitter complete')
+
+
+def looping_json_emitter_local(local_dir, min_job_number, descrip, *loopers, **kwargs):
+	buffer = create_csv_repeat_set(*loopers, filename=None, return_buffer=True)
+	jsons = json_emitter(buffer, **kwargs)
+	for jobnum, j in enumerate(jsons):
+		clog(f'local writing job {min_job_number+jobnum}')
+		with open(os.path.join(local_dir, f'{min_job_number+jobnum} {descrip}.json'), 'w') as jfile:
+			jfile.write(json.dumps(j))
+	clog(f'looping_json_emitter_local complete')
