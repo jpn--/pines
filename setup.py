9215a3fb35c4f9384bf2ef2e71c21865a883255c
Jeff Newman <jnewman@camsys.com>
2017-08-02 15:41:32 -0500

    Bump version: 1.32.0 → 1.33.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pines',
-    version='1.32',
+    version='1.33',
 
     description='A toolkit for various purposes',
     long_description=long_description,
