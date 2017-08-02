26abaed57c284c297a2e499ef846246ce1ad3882
Jeff Newman <jnewman@camsys.com>
2017-08-02 15:41:28 -0500

    relpath

--- pines/zipdir.py
+++ pines/zipdir.py
@@ -9,7 +9,7 @@ def _zipdir(path, ziph, skip_dots=True):
 		if (len(folder) and folder[0] != '.') or not skip_dots:
 			for file in files:
 				if file[0]!='.' or not skip_dots:
-					ziph.write(os.path.join(root, file))
+					ziph.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), os.path.join(path, '..')))
 
 def zipdir(source_dir, zip_file_name, skip_dots=True):
 	with zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
