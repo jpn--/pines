b7d54c4b8a7d81613fccb61f0db3b826e62d7155
Jeffrey Newman <jeff@newman.me>
2017-08-09 14:54:25 -0500

    Bump version: 1.110.0 → 1.111.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pines',
-    version='1.110',
+    version='1.111',
 
     description='A toolkit for various purposes',
     long_description=long_description,
