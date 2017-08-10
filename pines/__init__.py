cac58adc497ccb3a544b3ebe507382399f753be7
Jeffrey Newman <jeff@newman.me>
2017-08-09 21:46:00 -0500

    Bump version: 2.1.0 → 2.2.0

--- pines/__init__.py
+++ pines/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-__version__ = '2.1'
+__version__ = '2.2'
 
 def info():
     import sys, os
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pines',
-    version='2.1',
+    version='2.2',
 
     description='A toolkit for various purposes',
     long_description=long_description,
