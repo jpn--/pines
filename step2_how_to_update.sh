bd0cff147edb3ad84d0e7390111ba26e6a72fb33
Jeff Newman <jnewman@camsys.com>
2017-08-15 22:17:26 -0500

    Bump version: 2.23.0 → 2.24.0

--- step2_how_to_update.sh
+++ step2_how_to_update.sh
@@ -7,8 +7,8 @@ git push
 CONDAOUT=`conda build ./conda  --output-folder conda_builds/ --output`
 conda build ./conda --output-folder conda_builds/ -c jpn
 
-conda convert --platform win-64 conda_builds/osx-64/pines-2.23.0-*.tar.bz2 -o conda_builds/
-anaconda upload conda_builds/win-64/pines-2.23.0-*.tar.bz2
+conda convert --platform win-64 conda_builds/osx-64/pines-2.24.0-*.tar.bz2 -o conda_builds/
+anaconda upload conda_builds/win-64/pines-2.24.0-*.tar.bz2
 
 cd ${HOME}
 
