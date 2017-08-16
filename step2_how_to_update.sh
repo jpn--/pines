14497849b105ffc7caaf57a3be33ea60c3218de1
Jeff Newman <jnewman@camsys.com>
2017-08-15 22:17:20 -0500

    channel conda

--- step2_how_to_update.sh
+++ step2_how_to_update.sh
@@ -5,12 +5,12 @@ python setup.py bdist_wheel upload
 git push
 
 CONDAOUT=`conda build ./conda  --output-folder conda_builds/ --output`
-conda build ./conda --output-folder conda_builds/
+conda build ./conda --output-folder conda_builds/ -c jpn
 
 conda convert --platform win-64 conda_builds/osx-64/pines-2.23.0-*.tar.bz2 -o conda_builds/
 anaconda upload conda_builds/win-64/pines-2.23.0-*.tar.bz2
 
 cd ${HOME}
 
-pip install pines --no-cache-dir -U
+conda install pines -c jpn
 
