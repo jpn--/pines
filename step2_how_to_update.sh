51704387dbbd3cf2c732554337d0b29f6b615599
Jeff Newman <jnewman@camsys.com>
2017-08-15 21:28:34 -0500

    how_to_update in 2 steps

---
+++ step2_how_to_update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x -e
+
+python setup.py bdist_wheel upload
+
+git push
+
+CONDAOUT=`conda build ./conda  --output-folder conda_builds/ --output`
+conda build ./conda --output-folder conda_builds/
+
+conda convert --platform win-64 conda_builds/osx-64/pines-2.21.0-*.tar.bz2 -o conda_builds/
+anaconda upload conda_builds/win-64/pines-2.21.0-*.tar.bz2
+
+cd ${HOME}
+
+pip install pines --no-cache-dir -U
+
