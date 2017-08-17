b97e6a149a63083f7d80a4ece2b1343dcd502a7d
Jeff Newman <jnewman@camsys.com>
2017-08-16 20:51:23 -0500

    add counter

--- pines/counter.py
+++ pines/counter.py
@@ -10,3 +10,11 @@ class Counter(dicta):
 		else:
 			self[key] = 1
 
+	def add(self, other_counter):
+		for key, val in other_counter.items():
+			if key in self:
+				self[key] += val
+			else:
+				self[key] = val
+
+
