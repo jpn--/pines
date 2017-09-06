02fec29a32d4eb753e2c56657d8c7c76ad04576d
Jeff Newman <jnewman@camsys.com>
2017-09-06 12:40:57 -0500

    bugfix

--- pines/cluster.py
+++ pines/cluster.py
@@ -78,15 +78,11 @@ class Client(_Client):
 	def change_resources(self, worker, new_resources=None):
 
 		def delta_resource(worker_, resources, dask_scheduler=None):
-			dask_scheduler.remove_resources(w)
-			dask_scheduler.add_resources(stream=None, worker=w, resources=r )
-
 			# remove_resources(self, worker):
 			if worker_ in dask_scheduler.worker_resources:
 				del dask_scheduler.used_resources[worker_]
 				for resource, quantity in dask_scheduler.worker_resources.pop(worker_).items():
 					del dask_scheduler.resources[resource][worker_]
-
 			# add_resources(self, stream=None, worker=None, resources=None):
 			if worker_ not in dask_scheduler.worker_resources:
 				dask_scheduler.worker_resources[worker_] = resources.copy()
