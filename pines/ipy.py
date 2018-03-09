642ef1f7c9d3b58d68bb5b7d6177f200c80410a4
Jeffrey Newman <jeff@newman.me>
2018-03-09 12:58:10 -0600

    nb tools

--- pines/ipy.py
+++ pines/ipy.py
@@ -1,5 +1,7 @@
 
 from IPython.display import display_javascript
+from IPython.display import display, Javascript
+import os, time
 
 def make_markdown_cell(s):
 	display_javascript("""var t_cell = IPython.notebook.get_selected_cell()
@@ -9,4 +11,29 @@ def make_markdown_cell(s):
     IPython.notebook.get_cell(t_index).render();""".format(s.replace('\n' ,'\\n')), raw=True)
 
 
+def notebook_save():
+	display(Javascript('IPython.notebook.save_checkpoint();'))
+
+
+def notebook_execute(notebook_filename, kernel_name='Python 3.6', timeout=600, execution_dir='notebooks/', output_filemark=None):
+	import nbformat
+	from nbconvert.preprocessors import ExecutePreprocessor
+
+	if output_filemark is None:
+		output_filemark = time.strftime("%Y.%m.%d-%H.%M")
+
+	executed_notebook_filename = "{1}.{0}{2}".format(
+		output_filemark,
+		*os.path.splitext(notebook_filename)
+	)
+
+	with open(notebook_filename) as f:
+		nb = nbformat.read(f, as_version=4)
+
+	ep = ExecutePreprocessor(timeout=timeout, kernel_name=kernel_name)
+
+	ep.preprocess(nb, {'metadata': {'path': execution_dir}})
+
+	with open(executed_notebook_filename, 'wt') as f:
+		nbformat.write(nb, f)
 
