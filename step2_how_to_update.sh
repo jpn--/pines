5f86f520b09487f38f3f4df3ca12bd920a0899b0
Jeffrey Newman <jeff@newman.me>
2019-10-24 11:02:14 -0500

    Bump version: 2.91.0 → 2.92.0

--- step2_how_to_update.sh
+++ step2_how_to_update.sh
@@ -7,9 +7,9 @@ git push --tags
 
 conda build ./conda --output-folder conda_builds/ -c jpn --python 3.7
 
-conda convert --platform win-64 conda_builds/osx-64/pines-2.91.0-*.tar.bz2 -o conda_builds/
-anaconda upload conda_builds/win-64/pines-2.91.0-*.tar.bz2
+conda convert --platform win-64 conda_builds/osx-64/pines-2.92.0-*.tar.bz2 -o conda_builds/
+anaconda upload conda_builds/win-64/pines-2.92.0-*.tar.bz2
 
-conda convert --platform linux-64 conda_builds/osx-64/pines-2.91.0-*.tar.bz2 -o conda_builds/
-anaconda upload conda_builds/linux-64/pines-2.91.0-*.tar.bz2
+conda convert --platform linux-64 conda_builds/osx-64/pines-2.92.0-*.tar.bz2 -o conda_builds/
+anaconda upload conda_builds/linux-64/pines-2.92.0-*.tar.bz2
 
